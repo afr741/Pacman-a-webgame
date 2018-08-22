5f5a3d4dd133bd1b6aad13377b23790a8418d465
afr741 <afr741@mun.ca>
2018-08-21 22:18:18 -0230

    extra stuff

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 
 var score = 0; gscore= 0; ghost = false; 
+countblink = 10;
 var player = {
 	x:50,
 	y:100,
@@ -17,8 +18,20 @@ var enemy = {
 	ghostNum: 0,
 	moving: 0,
 	dirx:0,
-	diry:0
+	diry:0,
+	flash: 0,
+	ghosteat: false
 
+
+}
+
+
+var powerdot = {
+	x:10,
+	y:10,
+	powerup:false,
+	pcountdown:0,
+	ghostNum:0
 
 }
 
@@ -37,6 +50,10 @@ mainImage.src = "assets/pac.png"
 
 
 
+
+
+
+//Controls
 var keyclick = {};
 document.addEventListener("keydown", function(event) {
 	keyclick[event.keyCode]=true;
@@ -93,22 +110,36 @@ function myNum(n) {
 }
 
 
-
+//draw on canvas
 function render() {
 	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+
+
+
+//checks if powerup dot is on screen
+if(!powerdot.powerup && powerdot.pcountdown< 5) {
+	powerdot.x = myNum(420)+30;
+	powerdot.y = myNum(250)+30;
+	powerdot.powerup = true;
+}
+//check if ghost is on screen
 	if (!ghost){
 		enemy.ghostNum =  myNum(5) *64;
 		enemy.x = myNum(450);
 		enemy.y = myNum(250)+30;
 		ghost = true;
 	}
+
+//move enemy
 	if(enemy.moving <0) {
 		enemy.moving = (myNum(20)*3)+10+myNum(1);
 		enemy.speed = myNum(3)+1;
 		enemy.dirx = 0;
 		enemy.diry = 0;
+		if(powerdot.ghosteat){enemy.speed = enemy.speed*-1;}
+
 		if(enemy.moving%2){
 
 			if(player.x <enemy.x){enemy.dirx = -enemy.speed;}else{enemy.dirx = enemy.speed;}
@@ -129,17 +160,101 @@ function render() {
 	if (enemy.x < 0){enemy.x = (canvas.width-32);}
 	if (enemy.y < 0){enemy.y =(canvas.height-32);}
 
-	ctx.font = "20px Verdana";
-	ctx.fillStyle="white";
-	ctx.fillText("Pacman: "+score+" vs Ghost "+gscore,2, 18);
 
 
-	
-	ctx.drawImage(mainImage, enemy.ghostNum, 0, 32, 32, enemy.x, enemy.y, 32, 32);
-	ctx.drawImage(mainImage, player.pacmouth, player.pacdir, 32, 32, player.x, player.y, player.psize, player.psize);
-	
+
+
+
+//Collision detection GHOST
+if(player.x <= (enemy.x+26)  && enemy.x <=(player.x+26) && player.y <= (enemy.y+26)  && enemy.y <=(player.y +32)){
+	console.log('ghost');
+	if(powerdot.ghosteat){
+		score++;
+	}
+	else {
+		gscore++;
+	}
+player.x = 10;
+player.y = 100;
+enemy.x =300;
+enemy.y = 200;
+powerdot.pcountdown = 0;
+}
+
+if(powerdot.ghosteat){
+	powerdot.pcountdown--;
+	if(powerdot.pcountdown<=0){
+		powerdot.ghosteat = false;
+		enemy.ghostNum = powerdot.ghostNum;
+
+	}
+}
+
+
+
+//Collision detection POWERUP
+if(player.x <= powerdot.x  && powerdot.x <=(player.x+32) && player.y <= powerdot.y  && powerdot.y <=(player.y +32)){
+	console.log('hit');
+	powerdot.powerup = false;
+	powerdot.pcountdown = 500;
+	powerdot.ghostNum = enemy.ghostNum;
+	enemy.ghostNum =384;
+	powerdot.x = 0;
+	powerdot.y = 0;
+	powerdot.ghosteat = true;
+	player.speed = 10;
 
 
 }
+
+//powerup countdown
+if(powerdot.ghosteat){
+	powerdot.pcountdown--;
+	if(powerdot.pcountdown<=0){
+		powerdot.ghosteat = false;
+		enemy.ghostNum = powerdot.ghostNum;
+		player.speed = 5;
+
+	}
+}
+
+
+//draw powerupdot
+if(powerdot.powerup) {
+	ctx.fillStyle = "#ffffff";
+	ctx.beginPath();
+	ctx.arc(powerdot.x, powerdot.y, 10, 0, Math.PI *2, true);
+	ctx.closePath();
+	ctx.fill();
+}
+
+//enemy blinking
+if(countblink >0){
+	countblink--;
+}
+else{
+	countblink=10;
+}
+if(enemy.flash ==0){
+	enemy.flash = 32
+}
+
+else{
+	enemy.flash=0;
+}
+
+
+ctx.font = "20px Verdana";
+ctx.fillStyle = "white";
+ctx.fillText("Pacman: " + score + "vs Ghost: " + gscore, 2, 18);
+
+
+	ctx.drawImage(mainImage, enemy.ghostNum, enemy.flash, 32, 32, enemy.x, enemy.y, 32, 32);
+	ctx.drawImage(mainImage, player.pacmouth, player.pacdir, 32, 32, player.x, player.y, player.psize, player.psize);
+	
+
+}
+
+
 
 
